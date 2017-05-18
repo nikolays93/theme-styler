58fd03d979570909ce806e97d0b24c8d63b93c90
NikolayS93 <nikolays93@ya.ru>
2017-05-19 03:11:49 +0400

    must be sleep!

--- use-scss.php
+++ use-scss.php
@@ -23,6 +23,7 @@ Author URI: https://vk.com/nikolays_93
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
 if ( ! defined( 'ABSPATH' ) )
   exit; // disable direct access
 
@@ -39,54 +40,124 @@ if(!function_exists('is_wp_debug')){
   }
 }
 
-function remove_cyrillic_filter($str){
-  $pattern = "/[\x{0410}-\x{042F}]+.*[\x{0410}-\x{042F}]+/iu";
-  $str = preg_replace( $pattern, "", $str );
+if(!function_exists('remove_cyrillic_filter')){
+  function remove_cyrillic_filter($str){
+    $pattern = "/[\x{0410}-\x{042F}]+.*[\x{0410}-\x{042F}]+/iu";
+    $str = preg_replace( $pattern, "", $str );
 
-  return $str;
+    return $str;
+  }
 }
 
+define('COMPILER_OPT', 'wp-compiler');
+define('COMPILER_PLUG_DIR', plugin_dir_path( __FILE__ ) );
+define('SCSS_OPTION', 'scss');
+define('SCSS_CACHE', 'scss-cache');
+define('SCSS_DEFAULT_DIR', get_template_directory() . '/assets/scss/');
+define('ASSETS_DEFAULT_DIR', get_template_directory() . '/assets/');
+
+if(is_admin()){
+  require_once COMPILER_PLUG_DIR . '/inc/wp-admin-page-render/class-wp-admin-page-render.php';
+  require_once COMPILER_PLUG_DIR . '/inc/wp-form-render/class-wp-form-render.php';
+  require_once COMPILER_PLUG_DIR . '/inc/admin-page.php';
+}
+else {
+  require_once COMPILER_PLUG_DIR . '/inc/scss.inc.php';
+}
+
+//  - scss-cahce
+//  - scss-dir
+//  - scss-auto-compile
 function use_scss(){
-  $suffix = '.min';
-  if( is_wp_debug() !== false )
-    $suffix = '';
+  $options = get_option( COMPILER_OPT );
 
-  $options = get_option( DT_PLUGIN_NAME );
-  $scss_dir = $options['use-scss'];
-  $scss_cache = get_option( 'scss_cache' );
+  if( !current_user_can( apply_filters( 'scss_allow_role', 'administrator' ) ) ){
+    if( isset($_GET['force_scss']) )
+      wp_die( 'К сожалению, вам не разрешено компилировать SCSS.' );
+    return;
+  }
+  
+  if( isset($options['scss-auto-compile']) || isset($_GET['force_scss']) )
+    return;
 
-  // from
-  $file = get_template_directory() . $scss_dir . 'style.scss';
-  // to, suffix maybe has .min
-  $out_file = '/style'.$suffix.'.css';
-  $role = isset(wp_get_current_user()->roles[0]) ? wp_get_current_user()->roles[0] : '';
-  if($role == 'administrator'){
-    if( ! file_exists( $file ) )
-      return null;
+  $tpl_dir = get_template_directory();
+  /**
+   * Find Style SCSS 
+   */
+  $exists = array();
+  $scss_styles = array(
+    $tpl_dir . '/style.scss',
+    $tpl_dir . '/assets/style.scss',
+    SCSS_DEFAULT_DIR . 'style.scss');
 
-    if ( isset($_GET['force_scss']) || filemtime($file) !== $scss_cache ){
-      $scss = new scssc();
-      $scss->setImportPaths(function($path) {
-        if (!file_exists( apply_filters( 'SCSS_DIR', get_template_directory() . '/assets/' ).$path) )
-          return null;
-        return apply_filters( 'SCSS_DIR', get_template_directory() . '/assets/' ).$path;
-      });
+  foreach ($scss_styles as $scss_style) {
+    if( file_exists($scss_style) ){
+      $exists = array($scss_style);
+      break;
+    }
+  }
+  
+  /**
+   * Find Assets Files
+   */
+  if( isset($_GET['force_scss']) || apply_filters('assets_auto_compile', false ) ){
+    $handle = opendir(apply_filters( 'SCSS_DIR', SCSS_DEFAULT_DIR ));
+    while (false !== ($file = readdir( $handle ))){
+      if( strpos($file, '.scss') && substr($file, 0, 1) !== '_' ) 
+        $exists[] = apply_filters( 'SCSS_DIR', SCSS_DEFAULT_DIR ) . $file;
+    }
+  }
 
-      if(!is_wp_debug())
-        $scss->setFormatter('scss_formatter_compressed');
+  if( sizeof($exists) < 1 )
+    return;
+  
+  /**
+   * Compile SCSS
+   */
+  $compiler_loaded = false;
+  $old_cache = $scss_cache = get_option( SCSS_CACHE );
+  foreach ($exists as $exist) {
+    $cache_key = str_replace($tpl_dir, '', $exist);
 
-      $compiled = $scss->compile( remove_cyrillic_filter(file_get_contents($file)) );
+    if ( !isset($scss_cache[$cache_key]) || $scss_cache[$cache_key] !== filemtime($exist) ){
+      if( $compiler_loaded === false ){
+        $scss = new scssc();
+        $scss->setImportPaths(function($path) {
+          if (!file_exists( apply_filters( 'SCSS_DIR', SCSS_DEFAULT_DIR ) . $path) )
+            return null;
+          return apply_filters( 'SCSS_DIR', SCSS_DEFAULT_DIR ) . $path;
+        });
+        if(!is_wp_debug())
+          $scss->setFormatter('scss_formatter_compressed');
+
+        $compiler_loaded = true;
+      }
+
+      $compiled = $scss->compile( remove_cyrillic_filter(file_get_contents($exist)) );
+     
       if(!empty($compiled)){
-        file_put_contents(get_template_directory().$out_file, $compiled );
-        update_option( 'scss_cache', filemtime($file) );
-        $scss_cache = filemtime($file);
+        $out_dir = ($exist == $scss_style) ? $tpl_dir . '/' : apply_filters( 'ASSETS_DIR', ASSETS_DEFAULT_DIR );
+        $out_file = $out_dir . basename($cache_key, '.scss') . '.css';
+        file_put_contents( $out_file, $compiled );
+        $scss_cache[$cache_key] = filemtime($exist);
+        unset($compiled);
       }
     }
-  } // is user admin
-}
-// add_action('wp_enqueue_scripts', 'use_scss', 999 );
+  }
 
-add_action( 'admin_bar_menu', 'add_scss_menu', 99 );
+  /**
+   * Update Cache
+   */
+  if( $scss_cache != $old_cache )
+    update_option( SCSS_CACHE, $scss_cache );
+}
+add_action('wp_enqueue_scripts', 'use_scss', 999 );
+
+$options = get_option( COMPILER_OPT );
+
+if(!isset($options['scss-toolbar']))
+  add_action( 'admin_bar_menu', 'add_scss_menu', 99 );
+
 function add_scss_menu( $wp_admin_bar ) {
 
   $args = array(
@@ -111,10 +182,3 @@ function add_scss_menu( $wp_admin_bar ) {
   );
   $wp_admin_bar->add_node( $args );
 }
-
-// $dir = opendir (__DIR__);
-// while (false !== ($file = readdir($dir))) {
-//       if (strpos($file, '.scss') ) {
-//             var_dump($file);
-//       }
-// }
